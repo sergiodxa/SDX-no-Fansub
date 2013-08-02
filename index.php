2baddbaa25bb3ed95ec620247806d238628d9d18
sergiodxa <sergio@xalambri.com.ar>
2013-08-02 18:51:18 -0300

    primer subida de archivos

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Fansub CMS</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1" />
+	<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+	<script src="js/modernizr-2.5.3.min.js"></script>
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+</head>
+<body>
+	<header>
+		<figure id="logo">
+			<img src="/img/logo.png" alt="Logo de Fansub CMS" />
+		</figure><!--
+		--><h1 id="titulo">Fansub CMS</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="javascript:history.back(-1);" title="Volver atras" id="volver_nav">*</a></li>
+			<li><a href="#" title="Proyectos">Proyectos</a></li>
+			<li><a href="#" title="Blog">Blog</a></li>
+			<li><a href="#" title="Foro">Foro</a></li>
+			<li><a href="#" title="Preguntas frecuentes">FAQ</a></li>
+		</ul>
+	</nav>
+	<section id="proyectos">
+		<h2>Proyectos</h2>
+		<article class="proyecto">
+			<img src="/img/proyectos/proyecto.jpg" alt="Proyecto Portada" class="portada_proyecto" />
+			<h3 class="nombre_proyecto">Proyecto</h3>
+			<span class="estado_proyecto">Completo</span>
+			<span class="capitulos_proyecto">Capítulos: 12</span>
+			<a href="#" title="Ver todos los capítulos" class="ver_proyecto">Ver</a>
+		</article>
+		<article class="proyecto">
+			<img src="/img/proyectos/proyecto.jpg" alt="Proyecto Portada" class="portada_proyecto" />
+			<h3 class="nombre_proyecto">High School DxD New</h3>
+			<span class="estado_proyecto">Completo</span>
+			<span class="capitulos_proyecto">Capítulos: 12</span>
+			<a href="#" title="Ver todos los capítulos" class="ver_proyecto">Ver</a>
+		</article>
+	</section>
+	<footer>
+		<p>
+			<strong>Powered by SDX Studios</strong>
+		</p>
+	</footer>
+</body>
+</html>
