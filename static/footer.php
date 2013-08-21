71ab1dcea0f1fa1ba05b722cb96c0c703eaa9825
sergiodxa <sergio@xalambri.com.ar>
2013-08-21 20:24:52 -0300

    cambio en header, nav y footer

--- static/footer.php
+++ static/footer.php
@@ -1,8 +1,8 @@
 
 	<footer>
-		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.es">
+		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.es" id="licence">
 			<img alt="Licencia Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" />
-		</a><br />
+		</a>
 		<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">SDX no Fansub</span> por <a xmlns:cc="http://creativecommons.org/ns#" href="http://sdxstudios.com.ar" property="cc:attributionName" rel="cc:attributionURL">SDX Studios</a> se encuentra bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.es">Licencia Creative Commons Atribución-NoComercial-CompartirIgual 3.0 Unported</a>.
 		<a href="https://github.com/sergiodxa/SDX-no-Fansub" id="github" title="Repositorio en Github con el código"></a>
 	</footer>
